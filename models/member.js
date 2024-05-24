e864b5933655f6b3136c21ecebdf746a3bd1252f
shuantt <tehsuan.tht@gmail.com>
2024-05-24 11:54:28 +0800

    fix: 修改 會員 和 訂單 Model

--- models/member.js
+++ models/member.js
@@ -33,7 +33,7 @@ const memberSchema = new mongoose.Schema(
         },
         status: {
             type: Number,
-            default: 1,
+            default: 1,     // 0: : 停權 1: 啟用
         },
         point: {
             type: Number,
@@ -46,8 +46,7 @@ const memberSchema = new mongoose.Schema(
                     return v.every(interest => ['工藝手作', '烹飪烘烤', '藝術人文', '生活品味'].includes(interest));
                 },
                 message: props => `${props.value} 非有效 tags 值`
-            },
-            default: ['工藝手作']
+            }
         },
         collections: [{
             type: mongoose.Schema.Types.ObjectId,
--- models/order.js
+++ models/order.js
@@ -21,21 +21,39 @@ const orderSchema = new mongoose.Schema(
             ref: 'CourseItem',
             required: [true, "courseItemId 為必填"]
         },
-        itemName: {
+        // 顯示廠商名稱，避免廠商名稱更動後，訂單顯示不同名稱
+        vendorName: {
+            type: String,
+            required: [true, "vendorName 為必填"]
+        },
+        // 顯示品項名稱
+        courseItemName: {
             type: String,
             required: [true, "itemName 為必填"]
         },
+        // 訂購人數
         count: {
             type: Number,
             required: [true, "count 為必填"]
         },
+        // 單價
         price: {
             type: Number,
             required: [true, "price 為必填"]
         },
+        // 訂單總價
         totalPrice: {
             type: Number,
             required: [true, "totalPrice 為必填"]
+        },
+        // 上課日期，格式須為 Datetime 格式，用來確認是否已完課
+        courseTime: {
+            type: Date,
+            required: [true, "courseTime 為必填"]
+        },
+        // 教室地址
+        location: {
+            type: String,
         },
         paymentType: {
             type: Number,
@@ -47,12 +65,26 @@ const orderSchema = new mongoose.Schema(
             enum: [0, 1, 2], // 0:待付款, 1: 已付款, 2: 已取消
             default: 0,
         },
-        location: {
+        // 使用者送出後 5 碼後的時間
+        paidTime: {
+            type: Date,
+        },
+        // 賣家取消訂單時間
+        cancelTime: {
+            type: Date,
+        },
+        // 買家取消訂單原因
+        cancelReason: {
             type: String,
         },
+        // 訂單編號
         lastFiveDigits: {
             type: String,
         },
+        // 備註
+        note: {
+            type: String,
+        }
     },
     {
         timestamps: true,
