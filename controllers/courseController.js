905bfb9a8b81472294808a01119df8056e5654f5
sunny96087 <sunny96087@yahoo.com.tw>
2024-05-24 16:37:34 +0800

    feat: courseController 導入工具 & model

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,9 +1,12 @@
-const {Course} = require("../models/course");
+const Vendor = require("../models/vendor");
+const { Course, CourseItem, CourseComment } = require("../models/course");
 const appError = require("../utils/appError");
 const handleSuccess = require("../utils/handleSuccess");
+const { isAuth, generateSendJWT } = require("../utils/auth");
+const tools = require("../utils/tools");
 
 const courseController = {
-    // 取得課程列表
+    // 取得課程列表 (Front)
     getCourses: async (req, res, next) => {
         const courses = await Course.find();
         handleSuccess(res, courses, "取得課程列表成功");
