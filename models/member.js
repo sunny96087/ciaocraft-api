bdef8c69c481fdac21004a113552a3e87f6459ba
shuantt <tehsuan.tht@gmail.com>
2024-05-28 10:45:03 +0800

    feat: 新增 會員收藏 api、會員訂單 api 加入查詢條件
    1. 新增: 加入會員收藏 api
    2. 新增: 刪除會員收藏 api
    3. 新增: 取得會員訂單加入查詢條件
    4. 新增: 取得全部會員加入管理員密碼

--- models/member.js
+++ models/member.js
@@ -45,7 +45,7 @@ const memberSchema = new mongoose.Schema(
                 validator: function (v) {
                     return v.every(interest => ['工藝手作', '烹飪烘烤', '藝術人文', '生活品味'].includes(interest));
                 },
-                message: props => `${props.value} 非有效 tags 值`
+                message: props => `${props.value} 非有效 interests 值`
             }
         },
         collections: [{
